64f0c94baa4a79b5edd06b72aea1d5ff22de7a31
Abdulmajeedyusuf99 <Abdulmajeedyusuf99@gmail.com>
2025-07-03 17:40:55 +0100

    chore: improve code output

--- tax_calculator2.php
+++ tax_calculator2.php
@@ -1,21 +1,25 @@
 <?php
 function taxcalculator2() {
+    do{
+
     $salary=readline("Enter salary: ");
     if($salary > 100000){
         $percentage=($salary/50)*100;
         echo "tax = ". $percentage;
+        echo "\n";
     }
     if($salary >=80000 && $salary <=90000){
         $percentage=($salary/35)*100;
         echo"tax = ". $percentage;
+        echo "\n";
     }
     if($salary < 80000){
         $percentage=($salary/20)*100; 
         echo "tax = ".  $percentage;
+        echo "\n";
     }
-    else{
-        echo "invalid";
-    }
+    
+    }while($salary!==0);
 }
 taxcalculator2();
 
