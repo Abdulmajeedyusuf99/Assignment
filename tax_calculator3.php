55b66eb74b70b2a5d234acabb212cfeb23439496
Abdulmajeedyusuf99 <Abdulmajeedyusuf99@gmail.com>
2025-07-11 15:00:14 +0100

    chore: improve code output

--- tax_calculator3.php
+++ tax_calculator3.php
@@ -5,21 +5,21 @@ function taxcalculator3() {
     $salary=readline("Enter salary: ");
    switch($salary){
     case $salary > 100000:
-     $percentage=($salary/50)*100;
+     $percentage=(50/100)*$salary;
      echo "tax = ". $percentage;
      echo "\n";
 
      break;
 
     case $salary >=80000 && $salary <=90000:
-     $percentage=($salary/35)*100;
+     $percentage=(35/100)*$salary;
      echo"tax = ". $percentage;
     echo "\n";
 
      break;
 
     case $salary < 80000:
-    $percentage=($salary/20)*100;
+    $percentage=(20/100)*$salary;
     echo "tax = ". $percentage;
     echo "\n";
 
